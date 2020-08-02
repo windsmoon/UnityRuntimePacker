4db3482dc16e434c719f55cef5311a242a08433d
windsmoon <smwindsmoon@gmail.com>
2020-08-03 00:40:56 +0800

    implement basic functions #1

---
+++ UnityRuntimePacker/Assets/UnityRuntimePacker/Agents/AgentBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Windsmoon.UnityRuntimePacker.Agents
+{
+    public class AgentBase
+    {
+        #region methods
+        public virtual void Init()
+        {
+            PackerCore.Init();
+        }
+
+        public virtual Atlas GenerateAtlas(List<Texture2D> texture2DList)
+        {
+            return null;
+        }
+
+        public virtual void UnInit()
+        {
+            PackerCore.UnInit();
+        }
+        #endregion
+    }
+}
